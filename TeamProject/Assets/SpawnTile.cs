8ec34155110dd3a66ca2a8c56847ada838a27a16
SomeInternetUser <aleksandra_c@windowslive.com>
2016-03-16 00:46:05 +0100

    Added Initial Project

---
+++ TeamProject/Assets/SpawnTile.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+
+public class SpawnTile : MonoBehaviour {
+
+    List<Tile> tilesOnBoard = new List<Tile>();
+    List<Tile> tilesList = new List<Tile>(); //List of all disponible tiles (not on board)
+    Random rnd = new Random();
+
+    public Material startMaterial;
+    public Material m1;
+    public Material m2;
+    public Material m3;
+    public Material m4;
+
+    Ray myRay;   
+    RaycastHit hit;
+    public GameObject objectToinstantiate;
+    public int currentNbrTiles;
+
+    void Start()
+    {
+        currentNbrTiles = 0;
+        Tile startTile = new Tile();
+        startTile.Init(terrainTypes.castle, terrainTypes.grassRoad, terrainTypes.grassRoad, terrainTypes.grass, 0, 0, startMaterial);
+        tilesOnBoard.Add(startTile);
+
+        Tile t1 = new Tile();
+        t1.Init(terrainTypes.grassRoad, terrainTypes.grassRoad, terrainTypes.grassRoad, terrainTypes.grassRoad, 0, 0,m1);
+        tilesList.Add(t1);
+        Tile t2 = new Tile();
+        t2.Init(terrainTypes.grass, terrainTypes.grass, terrainTypes.grassRoad, terrainTypes.grass, 0, 0, m2);
+        tilesList.Add(t2);
+        Tile t3 = new Tile();
+        t3.Init(terrainTypes.grass, terrainTypes.grass, terrainTypes.grass, terrainTypes.grass, 0, 0, m3);
+        tilesList.Add(t3);
+        Tile t4 = new Tile();
+        t4.Init(terrainTypes.castle, terrainTypes.castle, terrainTypes.castle, terrainTypes.castle, 0, 0, m4);
+        tilesList.Add(t4);
+
+    }
+
+    void Update()
+    {
+        myRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(myRay, out hit))
+        { 
+
+            if (Input.GetMouseButtonDown(0))
+            {
+                if (tilesList.Count > 0)
+                {
+                    Vector3 position = hit.point;
+                    position.x = Mathf.Round(position.x / 10) * 10; // 10 is the size of a Tile
+                    position.z = Mathf.Round(position.z / 10) * 10; // 10 is the size of a Tile
+                    GameObject clone = Instantiate(objectToinstantiate, position, Quaternion.identity) as GameObject;// instatiate a prefab on the position where the ray hits the floor.
+                    int i = Random.Range(0, tilesList.Count);
+                    var item = tilesList[i];
+
+                    Tile tile = clone.AddComponent<Tile>();
+                    tile.Init(item.UpTerrain, item.RightTerrain, item.DownTerrain, item.LeftTerrain, position.x, position.z, item.Material);
+                    clone.GetComponent<Renderer>().material = item.Material;
+                    tilesList.RemoveAt(i);
+                    Debug.Log(hit.point);
+                    currentNbrTiles++;                  
+                }
+            }
+        }  
+    }
+}
---
+++ TeamProject/Assets/Tile.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+public enum terrainTypes
+{
+    grass,
+    castle,
+    grassRoad
+};
+public class Tile : MonoBehaviour {
+
+    
+    private terrainTypes upTerrain;
+    private terrainTypes rightTerrain;
+    private terrainTypes downTerrain;
+    private terrainTypes leftTerrain;
+    private Material material;
+    private float xPosition;
+    private float yPosition;
+
+    public void Init(terrainTypes up, terrainTypes right, terrainTypes down, terrainTypes left, float x, float y, Material m)
+    {
+        this.upTerrain = up;
+        this.rightTerrain = right;
+        this.downTerrain = down;
+        this.leftTerrain = left;
+        this.xPosition = x;
+        this.yPosition = y;
+        this.material = m;
+
+
+    }
+
+    public Material Material // This is your property
+    {
+        get { return this.material; }
+        
+    }
+
+    public terrainTypes UpTerrain // This is your property
+    {
+        get { return this.upTerrain; }
+
+    }
+    public terrainTypes RightTerrain // This is your property
+    {
+        get { return this.rightTerrain; }
+
+    }
+    public terrainTypes DownTerrain // This is your property
+    {
+        get { return this.downTerrain; }
+
+    }
+    public terrainTypes LeftTerrain // This is your property
+    {
+        get { return this.leftTerrain; }
+
+    }
+}
