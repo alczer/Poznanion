98b5fd71b7a4cf4aa39c99c2ea9fadcd7553afa0
Wojciechduda92 <wojciechduda_92@o2.pl>
2016-03-26 12:55:57 +0100

    Menu
    Podstawy interrfejsu.

---
+++ TeamProject/Assets/Menu.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using System.Collections;
+
+public class Menu : MonoBehaviour {
+
+    void OnGUI()
+    {
+        if (GUI.Button(new Rect(10, 10, 100, 50), "New Game"))
+        {
+
+            Application.LoadLevel("game");
+        }
+        if (GUI.Button(new Rect(10, 70, 100, 50), "Exit"))
+        {
+            Application.Quit();
+            Debug.Log("wyszedlo");
+        }
+
+    }
+	// Use this for initialization
+	void Start () {
+	Debug.Log("weszlo");
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+       
+	}
+}
