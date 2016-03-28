f7473d99ab10e37b711df0b79fd18bed81648b6a
SomeInternetUser <jusdorange@op.pl>
2016-03-29 01:56:11 +0200

    New GUI test

---
+++ TeamProject/Assets/UIManagerScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class UIManagerScript : MonoBehaviour {
+
+    public void StartGame()
+    {
+        //Application.LoadLevel("game");
+        SceneManager.LoadScene("game");
+    }
+
+    public void GoToMenu()
+    {
+        SceneManager.LoadScene("menu");
+
+    }
+
+}
