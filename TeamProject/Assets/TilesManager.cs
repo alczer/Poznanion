cb07989408eb171d867f561f2ebc61fdf71d3e89
Qitay <qitay2@gmail.com>
2016-04-09 21:46:07 +0200

    Next tile display interface, fixes

--- TeamProject/Assets/TilesManager.cs
+++ TeamProject/Assets/TilesManager.cs
@@ -206,66 +206,69 @@ public class TilesManager : MonoBehaviour {
         return possiblePositions.Distinct().ToList();
     }
     public void init ()
-        {
-
+    {
+        //RRRR
         addTileToList(terrainTypes.road, terrainTypes.road, terrainTypes.road, terrainTypes.road, 0, 0, RRRR, 1, terrainTypes.intersection, terrainTypes.grass, terrainTypes.road, terrainTypes.grass,
             terrainTypes.grass, terrainTypes.road, terrainTypes.grass, terrainTypes.grass, terrainTypes.road, terrainTypes.grass, terrainTypes.grass, terrainTypes.road, terrainTypes.grass,
             new List<List<int>>() { new List<int>() { 3, 4 }, new List<int>() { 6, 7 }, new List<int>() { 9, 10 }, new List<int>() { 12, 1 } }, new List<List<int>>() { },
             new List<List<int>>() { new List<int>() { 2 }, new List<int>() { 5 }, new List<int>() { 8 }, new List<int>() { 11 } });
+        //CCRC
         addTileToList(terrainTypes.castle, terrainTypes.castle, terrainTypes.road, terrainTypes.castle, 0, 0, CCRC, 1, terrainTypes.castle, terrainTypes.castle, terrainTypes.castle, terrainTypes.castle,
-             terrainTypes.castle, terrainTypes.castle, terrainTypes.castle, terrainTypes.grass, terrainTypes.road, terrainTypes.grass, terrainTypes.castle, terrainTypes.castle, terrainTypes.castle,
-             new List<List<int>>() { new List<int>() { 7 }, new List<int>() { 9 } }, new List<List<int>>() { new List<int>() { 1, 2, 3, 4, 5, 6, 10, 11, 12 } }, new List<List<int>>() { new List<int>() { 8 } });
+                terrainTypes.castle, terrainTypes.castle, terrainTypes.castle, terrainTypes.grass, terrainTypes.road, terrainTypes.grass, terrainTypes.castle, terrainTypes.castle, terrainTypes.castle,
+                new List<List<int>>() { new List<int>() { 7 }, new List<int>() { 9 } }, new List<List<int>>() { new List<int>() { 1, 2, 3, 4, 5, 6, 10, 11, 12 } }, new List<List<int>>() { new List<int>() { 8 } });
+        //CCFC
         addTileToList(terrainTypes.castle, terrainTypes.castle, terrainTypes.grass, terrainTypes.castle, 0, 0, CCFC, 3, terrainTypes.castle, terrainTypes.castle, terrainTypes.castle, terrainTypes.castle,
-             terrainTypes.castle, terrainTypes.castle, terrainTypes.castle, terrainTypes.grass, terrainTypes.grass, terrainTypes.grass, terrainTypes.castle, terrainTypes.castle, terrainTypes.castle,
-             new List<List<int>>() { new List<int>() { 7, 8, 9 } }, new List<List<int>>() { new List<int>() { 1, 2, 3, 4, 5, 6, 10, 11, 12 } }, new List<List<int>>() { new List<int>() { } });
+                terrainTypes.castle, terrainTypes.castle, terrainTypes.castle, terrainTypes.grass, terrainTypes.grass, terrainTypes.grass, terrainTypes.castle, terrainTypes.castle, terrainTypes.castle,
+                new List<List<int>>() { new List<int>() { 7, 8, 9 } }, new List<List<int>>() { new List<int>() { 1, 2, 3, 4, 5, 6, 10, 11, 12 } }, new List<List<int>>() { new List<int>() { } });
+        //CFFF
         addTileToList(terrainTypes.castle, terrainTypes.grass, terrainTypes.grass, terrainTypes.grass, 0, 0, CFFF, 5, terrainTypes.grass, terrainTypes.castle, terrainTypes.castle, terrainTypes.castle,
             terrainTypes.grass, terrainTypes.grass, terrainTypes.grass, terrainTypes.grass, terrainTypes.grass, terrainTypes.grass, terrainTypes.grass, terrainTypes.grass, terrainTypes.grass,
             new List<List<int>>() { new List<int>() { 4, 5, 6, 7, 8, 9, 10, 11, 12 } }, new List<List<int>>() { new List<int>() { 1, 2, 3 } }, new List<List<int>>() { });
 
-        /*
-            //public Material CRFR; 4 - START
-            addTileToList(terrainTypes.castle, terrainTypes.grassRoad, terrainTypes.grass, terrainTypes.grassRoad, 0, 0, CRFR, 3);
-            //public Material CFFF; 
-            addTileToList(terrainTypes.castle, terrainTypes.grass, terrainTypes.grass, terrainTypes.grass, 0, 0, CFFF);
-            //public Material CFFC;
-            addTileToList(terrainTypes.castle, terrainTypes.grass, terrainTypes.grass, terrainTypes.castle, 0, 0, CFFC);
-            //public Material CFFC_2;
-            addTileToList(terrainTypes.castle, terrainTypes.grass, terrainTypes.grass, terrainTypes.castle, 0, 0, CFFC_2);
-            //public Material CFFC_P;
-            addTileToList(terrainTypes.castle, terrainTypes.grass, terrainTypes.grass, terrainTypes.castle, 0, 0, CFFC_P);
-            //public Material CCFC;
-            addTileToList(terrainTypes.castle, terrainTypes.castle, terrainTypes.grassRoad, terrainTypes.castle, 0, 0, CCFC);
-            //public Material CCFC_P;
-            addTileToList(terrainTypes.castle, terrainTypes.grassRoad, terrainTypes.grassRoad, terrainTypes.grassRoad, 0, 0, CCFC_P);
-            //public Material CCRC;
-            addTileToList(terrainTypes.castle, terrainTypes.grassRoad, terrainTypes.grassRoad, terrainTypes.grassRoad, 0, 0, CCRC);
-            //public Material CCRC_P;
-            addTileToList(terrainTypes.castle, terrainTypes.grassRoad, terrainTypes.grassRoad, terrainTypes.grassRoad, 0, 0, CCRC_P);
-            //public Material CCCC_P;
-            addTileToList(terrainTypes.castle, terrainTypes.grassRoad, terrainTypes.grassRoad, terrainTypes.grassRoad, 0, 0, CCCC_P);
-            //public Material CRRC;
-            addTileToList(terrainTypes.castle, terrainTypes.grassRoad, terrainTypes.grassRoad, terrainTypes.grassRoad, 0, 0, CRRC);
-            //public Material CRRC_P;
-            addTileToList(terrainTypes.grass, terrainTypes.grassRoad, terrainTypes.grassRoad, terrainTypes.grassRoad, 0, 0, CRRC_P);
-            //public Material FCFC;
-            addTileToList(terrainTypes.grass, terrainTypes.grassRoad, terrainTypes.grassRoad, terrainTypes.grassRoad, 0, 0, FCFC);
-            //public Material FCFC_P;
-            addTileToList(terrainTypes.castle, terrainTypes.grassRoad, terrainTypes.grassRoad, terrainTypes.grassRoad, 0, 0, FCFC_P);
-            //public Material CFCF_2;
-            addTileToList(terrainTypes.castle, terrainTypes.grassRoad, terrainTypes.grassRoad, terrainTypes.grassRoad, 0, 0, CFCF_2);
-            //public Material CRRF;
-            addTileToList(terrainTypes.castle, terrainTypes.grassRoad, terrainTypes.grassRoad, terrainTypes.grassRoad, 0, 0, CRRF);
-            //public Material CFRR;
-            addTileToList(terrainTypes.castle, terrainTypes.grassRoad, terrainTypes.grassRoad, terrainTypes.grassRoad, 0, 0, CFRR);
-            //public Material CRRR;
-            addTileToList(terrainTypes.grassRoad, terrainTypes.grassRoad, terrainTypes.grassRoad, terrainTypes.grassRoad, 0, 0, CRRR);
-            //public Material FRFR;
-            //public Material FFRR;
-            //public Material FRRR;
-            //public Material RRRR;
-            //public Material FFRF_M;
-            //public Material FFFF_M;
-            */
+    /*
+        //public Material CRFR; 4 - START
+        addTileToList(terrainTypes.castle, terrainTypes.grassRoad, terrainTypes.grass, terrainTypes.grassRoad, 0, 0, CRFR, 3);
+        //public Material CFFF; 
+        addTileToList(terrainTypes.castle, terrainTypes.grass, terrainTypes.grass, terrainTypes.grass, 0, 0, CFFF);
+        //public Material CFFC;
+        addTileToList(terrainTypes.castle, terrainTypes.grass, terrainTypes.grass, terrainTypes.castle, 0, 0, CFFC);
+        //public Material CFFC_2;
+        addTileToList(terrainTypes.castle, terrainTypes.grass, terrainTypes.grass, terrainTypes.castle, 0, 0, CFFC_2);
+        //public Material CFFC_P;
+        addTileToList(terrainTypes.castle, terrainTypes.grass, terrainTypes.grass, terrainTypes.castle, 0, 0, CFFC_P);
+        //public Material CCFC;
+        addTileToList(terrainTypes.castle, terrainTypes.castle, terrainTypes.grassRoad, terrainTypes.castle, 0, 0, CCFC);
+        //public Material CCFC_P;
+        addTileToList(terrainTypes.castle, terrainTypes.grassRoad, terrainTypes.grassRoad, terrainTypes.grassRoad, 0, 0, CCFC_P);
+        //public Material CCRC;
+        addTileToList(terrainTypes.castle, terrainTypes.grassRoad, terrainTypes.grassRoad, terrainTypes.grassRoad, 0, 0, CCRC);
+        //public Material CCRC_P;
+        addTileToList(terrainTypes.castle, terrainTypes.grassRoad, terrainTypes.grassRoad, terrainTypes.grassRoad, 0, 0, CCRC_P);
+        //public Material CCCC_P;
+        addTileToList(terrainTypes.castle, terrainTypes.grassRoad, terrainTypes.grassRoad, terrainTypes.grassRoad, 0, 0, CCCC_P);
+        //public Material CRRC;
+        addTileToList(terrainTypes.castle, terrainTypes.grassRoad, terrainTypes.grassRoad, terrainTypes.grassRoad, 0, 0, CRRC);
+        //public Material CRRC_P;
+        addTileToList(terrainTypes.grass, terrainTypes.grassRoad, terrainTypes.grassRoad, terrainTypes.grassRoad, 0, 0, CRRC_P);
+        //public Material FCFC;
+        addTileToList(terrainTypes.grass, terrainTypes.grassRoad, terrainTypes.grassRoad, terrainTypes.grassRoad, 0, 0, FCFC);
+        //public Material FCFC_P;
+        addTileToList(terrainTypes.castle, terrainTypes.grassRoad, terrainTypes.grassRoad, terrainTypes.grassRoad, 0, 0, FCFC_P);
+        //public Material CFCF_2;
+        addTileToList(terrainTypes.castle, terrainTypes.grassRoad, terrainTypes.grassRoad, terrainTypes.grassRoad, 0, 0, CFCF_2);
+        //public Material CRRF;
+        addTileToList(terrainTypes.castle, terrainTypes.grassRoad, terrainTypes.grassRoad, terrainTypes.grassRoad, 0, 0, CRRF);
+        //public Material CFRR;
+        addTileToList(terrainTypes.castle, terrainTypes.grassRoad, terrainTypes.grassRoad, terrainTypes.grassRoad, 0, 0, CFRR);
+        //public Material CRRR;
+        addTileToList(terrainTypes.grassRoad, terrainTypes.grassRoad, terrainTypes.grassRoad, terrainTypes.grassRoad, 0, 0, CRRR);
+        //public Material FRFR;
+        //public Material FFRR;
+        //public Material FRRR;
+        //public Material RRRR;
+        //public Material FFRF_M;
+        //public Material FFFF_M;
+        */
     }
 
 
