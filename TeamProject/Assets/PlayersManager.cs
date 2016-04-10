a94262f4c5e0056c9b5a271de5942710e9a33ed6
SomeInternetUser <jusdorange@op.pl>
2016-04-10 23:38:16 +0200

    Added players

---
+++ TeamProject/Assets/PlayersManager.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PlayersManager : MonoBehaviour {
+    
+    public List<Player> tilesList = new List<Player>();
+
+}
