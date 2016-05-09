56b03a6058e8b10c11c690eb0f1490b41f673013
Qitay <qitay2@gmail.com>
2016-05-09 23:28:00 +0200

    Small camera update

--- TeamProject/Assets/CameraManager.cs
+++ TeamProject/Assets/CameraManager.cs
@@ -3,18 +3,63 @@ using System.Collections;
 
 public class CameraManager : MonoBehaviour {
 
-    public MoveCamera moveCamera;
-    public float maxX = 0;
-    public float minX = 0;
-    public float maxZ = 0;
-    public float minZ = 0;
+    // checks if camera limits need to be updated
     public bool cameraCheck = false;
+    
+    private float maxX = 0;
+    private float minX = 0;
+    private float maxZ = 0;
+    private float minZ = 0;
+
+    // rotation
+    public float turnSpeed = 4.0f;
+
+    // panning
+    public float panSpeed = 4.0f;
+    public float touchpanSpeed = 0.5f;
+
+    // middle button zoom
+    public float zoomSpeed = 4.0f;
+
+    // scroll zoom
+    public float scrollSpeed = 40f;
+
+    // pinch zoom
+    public float pinchZoomSpeed = 0.5f;
+
+    // edge move speed
+    public float edgeSpeed = 2f;
+
+    // movement
+    public static float Xmax = 10f;
+    public static float Xmin = -10f;
+    public static float Zmax = 10f;
+    public static float Zmin = -10f;
+
+    // distance
+    public float cameraDistanceMax = 90f;
+    public float cameraDistanceMin = 30f;
+
+    // movelimit increase value
+    public float moveIncrease = 10f;
+
+    private float zoom;
+
+    private Vector3 mouseOrigin;	// Position of cursor when mouse dragging starts
+
+    private bool isPanning;		    // Is the camera being panned?
+    private bool isRotating;	    // Is the camera being rotated?
+    private bool isZooming;         // Is the camera zooming (middlebutton)?
+
+    private bool dontUseTouch = true;   // Use touchscreen, or not
+    private bool useEdgePanning = false;
+
     public void UpdateCamera()
     {
-        moveCamera.Xmax = 40 + maxX * 4;
-        moveCamera.Xmin = -40 + minX * 4;
-        moveCamera.Zmax = 40 - minZ * 4;
-        moveCamera.Zmin = -40 - maxZ * 4;
+        Xmax = 10 + maxX * moveIncrease;
+        Xmin = -10 + minX * moveIncrease;
+        Zmax = 10 - minZ * moveIncrease;
+        Zmin = -10 - maxZ * moveIncrease;
     }
 
     public void CheckCamera(int[] x)
@@ -29,11 +74,169 @@ public class CameraManager : MonoBehaviour {
             minZ = x[0] - 100;
 
         cameraCheck = true;
-        //Debug.Log(x[0]);
-        //Debug.Log(x[1]);
-        //Debug.Log(maxX);
-        //Debug.Log(minX);
-        //Debug.Log(maxZ);
-        //Debug.Log(minZ);
+    }
+
+    //
+    // START
+    //
+    void Start()
+    {
+        Debug.Log("DPI =" + Screen.dpi);
+        
+        //check if our current system info equals a desktop
+        if (SystemInfo.deviceType == DeviceType.Desktop)
+        {
+            //we are on a desktop device, so don't use touch
+            dontUseTouch = true;
+        }
+        //if it isn't a desktop, lets see if our device is a handheld device aka a mobile device
+        else if (SystemInfo.deviceType == DeviceType.Handheld && Input.multiTouchEnabled)
+        {
+            //we are on a mobile device, so lets use touch input
+            dontUseTouch = false;
+        }
+    }
+
+    //
+    // UPDATE
+    //
+    void Update()
+    {
+        // Check current zoom
+        zoom = Camera.main.transform.position.y * 0.01f;
+
+        if (dontUseTouch)
+        {
+            // Get the left mouse button + LeftCTRL
+            if (Input.GetMouseButtonDown(0) && Input.GetKey(KeyCode.LeftControl))
+            {
+                // Get mouse origin
+                mouseOrigin = Input.mousePosition;
+                isRotating = true;
+            }
+            // Get the right mouse button
+            if (Input.GetMouseButtonDown(1))
+            {
+                // Get mouse origin
+                mouseOrigin = Input.mousePosition;
+                isPanning = true;
+            }
+            // Get the middle mouse button
+            if (Input.GetMouseButtonDown(2))
+            {
+                // Get mouse origin
+                mouseOrigin = Input.mousePosition;
+                isZooming = true;
+            }
+
+            // Disable movements on button release
+            if (!Input.GetMouseButton(0)) isRotating = false;
+            if (!Input.GetMouseButton(1)) isPanning = false;
+            if (!Input.GetMouseButton(2)) isZooming = false;
+
+            // Edge panning
+            if (!isZooming && !isRotating && !isPanning && useEdgePanning)
+            {
+                Vector3 tmp = (Camera.main.ScreenToViewportPoint(Input.mousePosition));
+                if (tmp.x > 0.99)
+                    transform.Translate(edgeSpeed * zoom, 0, 0);
+                else
+                    if (tmp.x < 0.01)
+                        transform.Translate(-edgeSpeed * zoom, 0, 0);
+                if (tmp.y > 0.99)
+                    transform.Translate(0, edgeSpeed * zoom, 0);
+                else
+                    if (tmp.y < 0.01)
+                        transform.Translate(0, -edgeSpeed * zoom, 0);
+            }
+
+            // Rotate camera along X and Y axis
+            if (isRotating)
+            {
+                Vector2 prevPos = Camera.main.ScreenToViewportPoint(Input.mousePosition);
+                Vector3 pos = Camera.main.ScreenToViewportPoint(Input.mousePosition - mouseOrigin);
+
+                transform.RotateAround(transform.position, transform.right, -pos.y * turnSpeed);
+                transform.RotateAround(transform.position, Vector3.up, pos.x * turnSpeed);
+            }
+
+            // Move the camera on it's XY plane
+            if (isPanning)
+            {
+                Vector3 pos = Camera.main.ScreenToViewportPoint(Input.mousePosition - mouseOrigin);
+
+                Vector3 move = new Vector3(-pos.x * panSpeed, -pos.y * panSpeed, 0);
+                transform.Translate(move, Space.Self);
+            }
+
+            // W A S D controls
+            if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+                transform.Translate(edgeSpeed * zoom, 0, 0);
+            if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+                transform.Translate(-edgeSpeed * zoom, 0, 0);
+            if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+                transform.Translate(0, edgeSpeed * zoom, 0);
+            if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
+                transform.Translate(0, -edgeSpeed * zoom, 0);
+
+            // middle button zoom
+            if (isZooming)
+            {
+                Vector3 pos = Camera.main.ScreenToViewportPoint(Input.mousePosition - mouseOrigin);
+
+                Vector3 move = pos.y * zoomSpeed * transform.forward;
+                transform.Translate(move, Space.World);
+            }
+
+            // ScrollMouse Zoom
+            if (!isZooming)
+            {
+                transform.Translate(0, 0, Input.GetAxis("Mouse ScrollWheel") * scrollSpeed);
+            }
+        }
+        // Multitouch controls
+        else
+        {
+            // Touchscreen panning
+            if (Input.touchCount == 1 && Input.GetTouch(0).phase == TouchPhase.Moved)
+            {
+                Vector2 touchDeltaPosition = Input.GetTouch(0).deltaPosition;
+                transform.Translate(-touchDeltaPosition.x * touchpanSpeed * zoom / Screen.dpi * 300,
+                                    -touchDeltaPosition.y * touchpanSpeed * zoom / Screen.dpi * 300, 0);
+            }
+
+            // Pinch Zoom
+            if (Input.touchCount == 2)
+            {
+                // Store both touches.
+                Touch touchZero = Input.GetTouch(0);
+                Touch touchOne = Input.GetTouch(1);
+
+                // Find the position in the previous frame of each touch.
+                Vector2 touchZeroPrevPos = touchZero.position - touchZero.deltaPosition;
+                Vector2 touchOnePrevPos = touchOne.position - touchOne.deltaPosition;
+
+                // Find the magnitude of the vector (the distance) between the touches in each frame.
+                float prevTouchDeltaMag = (touchZeroPrevPos - touchOnePrevPos).magnitude;
+                float touchDeltaMag = (touchZero.position - touchOne.position).magnitude;
+
+                // Find the difference in the distances between each frame.
+                float deltaMagnitudeDiff = prevTouchDeltaMag - touchDeltaMag;
+
+
+                transform.Translate(0, 0, -deltaMagnitudeDiff * pinchZoomSpeed);
+            }
+        }
+       
+
+        // limits
+        transform.position = new Vector3(
+        Mathf.Clamp(transform.position.x, Xmin, Xmax),
+        Mathf.Clamp(transform.position.y, cameraDistanceMin, cameraDistanceMax),
+        Mathf.Clamp(transform.position.z, Zmin, Zmax)
+            );
     }
 }
+
+
+
